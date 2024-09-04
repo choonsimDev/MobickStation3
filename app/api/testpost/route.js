0d5aa0ebf67c4d02fc54d2fd397d3261701db19b
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2024-09-04 15:56:51 +0900

    DB create product

--- app/api/testpost/route.js
+++ app/api/testpost/route.js
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function POST(req) {
     console.log("POST request received");
@@ -13,7 +16,37 @@ export async function POST(req) {
         storeName,
     } = data;
     console.log({ name, description, price, mileage, stock, imageUrl, categoryName, storeName });
-    return new Response('Hello, Next.js!', {
-        status: 200,
-    })
+    try {
+        const product = await prisma.product.create({
+            data: {
+                name,
+                description,
+                price,
+                mileage,
+                stock,
+                imageUrl,
+                category: {
+                    connect: {
+                        name: categoryName, // Category 테이블에서 name으로 연결
+                    },
+                },
+                store: {
+                    connect: {
+                        name: storeName, // Store 테이블에서 name으로 연결
+                    },
+                },
+            },
+        });
+
+        return new Response(JSON.stringify(product), { status: 201 });
+
+    } catch (error) {
+        console.error("Failed to add product:", error);
+        return new Response(JSON.stringify({ error: "Failed to add product" }), {
+            status: 500,
+        });
+    }
+    // return new Response('Hello, Next.js!', {
+    //     status: 200,
+    // })
 }
